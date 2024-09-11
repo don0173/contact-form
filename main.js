3caf583e5d4317a1c2a01362cdaf9f16e81de825
don0173 <158238431+don0173@users.noreply.github.com>
2024-09-10 17:49:59 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function(){
+  "use strict":
+  window.addEventListener("load"),
+    function(){
+    var form = document.getElementById("contactForm");
+  console.log(form):
+  form.addEventListener("submit")<
+  }
+  }
